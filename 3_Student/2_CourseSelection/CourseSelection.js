66bf06762132baf9f76a3b2fdf7b2ae02fcf4ac3
Jenittah Suthakaran <iamjenittahs@gmail.com>
2024-10-18 06:11:29 +0530

    Update CourseSelection.js
    #JS Final Commit

--- 3_Student/2_CourseSelection/CourseSelection.js
+++ 3_Student/2_CourseSelection/CourseSelection.js
@@ -27,6 +27,8 @@ async function GetAllCourses() {
 };
 GetAllCourses();
 
+
+
 const StudentCourseEnrollADDURL = 'http://localhost:5209/api/student/course-selection?nic=';
 async function StudentCourseEnrollAdd(Nic,data){
     await fetch(`${StudentCourseEnrollADDURL}${Nic}`,{
@@ -40,18 +42,21 @@ async function StudentCourseEnrollAdd(Nic,data){
 
 let uniqueCourses = [];
 
-for (let i = 0; i < courses.length; i++) {
+function DuplicateCourseRemove(){
+    // Remove Duplicates courses from Database
+    for (let i = 0; i < courses.length; i++) {
 
-    for (let k = 0; k < 1; k++) {
-        let course = courses[i].courseName;
-        
-        if (!uniqueCourses.includes(course)) {
-            uniqueCourses.push(course);
+        for (let k = 0; k < 1; k++) {
+            let course = courses[i].coursename;
+
+            if (!uniqueCourses.includes(course)) {
+                uniqueCourses.push(course);
+            }
         }
     }
     CourseDropDown();
-
 }
+
 function CourseDropDown() {
     uniqueCourses.forEach(C => {
         const courseDropDown = document.createElement("option")
@@ -77,42 +82,41 @@ function StudentNameShow() {
 //disabled nic input 
 document.getElementById("nic").disabled = true
 
-
 //submit form
-document.getElementById("course-form").addEventListener('submit',(event)=>{
+document.getElementById("course-form").addEventListener('submit', (event) => {
     event.preventDefault()
 
     //Get form values
-    const nicNumber = document.getElementById("nic").value; 
-    const course = document.getElementById("select-course").value; 
-    const ProficiencyLevels = document.getElementById("proficiency-levels").value; 
-    const duration = document.getElementById("select-duration").value; 
+    const nicNumber = document.getElementById("nic").value;
+    const course = document.getElementById("select-course").value;
+    const ProficiencyLevels = document.getElementById("proficiency-levels").value;
+    const duration = document.getElementById("select-duration").value;
 
-     //Find the Student 
-     const student = students.find(s => s.nic == nicNumber);
-     console.log(student)
- 
- 
-     if (student) {
-         if (course != "" && ProficiencyLevels != "" && duration != "") {
-             const data={
-                 course:course,
-                 proficiencyLevels:ProficiencyLevels,
-                 duration:duration
-             }
-             StudentCourseEnrollAdd(nicNumber,data)
-             document.getElementById('message').style.color = "green"
-             document.getElementById('message').textContent = "Course Successfuly selected"
- 
-             sessionStorage.setItem('nic', JSON.stringify(nicNumber))
-             
-             setTimeout(()=>{
-                 window.location.href = "../3_Dashboard/StudentDashboard.html"
-             }, 500);
-         } else {
-             document.getElementById('message').style.color = "red";
-             document.getElementById('message').textContent = `Please Choose the field`;
-         }
-     }
- })
+    //Find the Student 
+    const student = students.find(s => s.nic == nicNumber);
+    console.log(student)
+
+
+    if (student) {
+        if (course != "" && ProficiencyLevels != "" && duration != "") {
+            const data={
+                course:course,
+                proficiencyLevels:ProficiencyLevels,
+                duration:duration
+            }
+            StudentCourseEnrollAdd(nicNumber,data)
+            document.getElementById('message').style.color = "green"
+            document.getElementById('message').textContent = "Course Successfuly selected"
+
+            sessionStorage.setItem('nic', JSON.stringify(nicNumber))
+            
+            setTimeout(()=>{
+                window.location.href = "../3_Dashboard/StudentDashboard.html"
+            }, 500);
+        } else {
+            document.getElementById('message').style.color = "red";
+            document.getElementById('message').textContent = `Please Choose the field`;
+        }
+    }
+})
 
