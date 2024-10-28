0bad2a96fe09d4204fa4603a51a6e1b407a47014
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-28 18:37:13 +0530

    contact us data get from input

--- 1_Home/Contact/Contact.js
+++ 1_Home/Contact/Contact.js
@@ -1,4 +1,21 @@
 document.getElementById("admin-btn").addEventListener("click",()=>{
-    window.location.href = "../../2_Admin/1_AdminLogin/AdminLogin.html"
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const today = new Date();
+    let id = Number(Math.floor(Math.random()*1000000))
+
+    const ContactUsDetails = {
+        id,
+        name,
+        email,
+        message,
+        submitDate:today
+    }
+
+    AddContactUs(ContactUsDetails)
+    event.target.reset();
 });
 
