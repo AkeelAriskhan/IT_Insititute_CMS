591b8310bdd3b870b0ffd3773f1f10ffaa8008d9
AkeelAriskhan <akeelariskhan1314@gmail.com>
2024-10-28 19:01:02 +0530

    contact us data

--- 1_Home/Contact/Contact.js
+++ 1_Home/Contact/Contact.js
@@ -1,9 +1,24 @@
-document.getElementById("admin-btn").addEventListener("click",()=>{
+const ContactUsURL = 'http://localhost:5209/api/ContactUs/Add-ContactUs-Details';
+
+async function AddContactUs(ContactUsDetails){
+    await fetch((ContactUsURL),{
+        method:'POST',
+        headers:{
+           "Content-Type": "application/json"
+        },
+        body:JSON.stringify(ContactUsDetails)
+    }).catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
+
+
+document.getElementById("contactus").addEventListener("submit",()=>{
     event.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('lname').value;
+    const message = document.getElementById('subject').value;
     const today = new Date();
     let id = Number(Math.floor(Math.random()*1000000))
 
